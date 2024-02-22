8a317110e4dcc7127abd39844de740836a47cceb
“parthvaghela99” <pmwaghela2003@gmail.com>
2024-02-22 15:01:34 +0530

    model

---
+++ User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class User
+    {
+        public int c_id{ get; set; }
+    }
+}
